2565d3c72537fcdacd87c42dc778fe6656e2c1dc
vuphongle <lephong29092909@gmail.com>
2025-02-27 01:27:23 +0700

    Create CartPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
+import { CartPage } from './pages/CartPage';
 import { ToastContainer } from 'react-toastify';
 import { HomePage } from './pages/HomePage';
 
@@ -17,6 +18,9 @@ function App() {
         <Route path='/register' element={<RegisterPage />} />
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
         
+        {/* Shopping */}
+        <Route path="/cart" element={<CartPage />} />
+
       </Routes>
     </BrowserRouter>
     
